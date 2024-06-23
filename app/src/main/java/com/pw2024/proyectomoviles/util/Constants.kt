305d2a60a2d0a83c60d3281055cc34760da5cd67
Diego Urbina <00544424@uca.edu.sv>
2024-06-22 22:02:45 -0600

    chore: Update app icon

---
+++ app/src/main/java/com/pw2024/proyectomoviles/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.pw2024.proyectomoviles.util
+
+object Constants {
+    //Serialized names
+    const val DATE_PUBLISHED = "datePublished"
+    const val PUBLISHED_BY = "publishedBy"
+    const val BOOK = "book"
+    const val TRANSACTION_TYPE = "transactionType"
+    const val IMAGE = "image"
+}
