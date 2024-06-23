d3ab069b2c7c0c3e5aab5e9e898f5c589f203f8f
golic <00214917@uca.edu.sv>
2024-06-23 02:38:08 -0600

    update main activity

--- app/src/main/java/com/pw2024/proyectomoviles/MainActivity.kt
+++ app/src/main/java/com/pw2024/proyectomoviles/MainActivity.kt
@@ -4,14 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.pw2024.proyectomoviles.model.navigate.BottomNavigation
-import com.pw2024.proyectomoviles.model.navigate.NavBarGraph
-import com.pw2024.proyectomoviles.model.navigate.NavBarItemList
-import com.pw2024.proyectomoviles.ui.register.Register
 import com.pw2024.proyectomoviles.ui.theme.ProyectoMovilesTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,29 +13,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProyectoMovilesTheme {
 
-                val navController = rememberNavController()
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentRoute: String? = navBackStackEntry?.destination?.route
-                val navItems = NavBarItemList()
-
-                Scaffold(
-                    bottomBar = {
-                        BottomNavigation(items = navItems, currentRoute = currentRoute) {
-                                currentNavigationItem ->
-                            navController.navigate(currentNavigationItem.route){
-                                navController.graph.startDestinationRoute?.let{startDestinationRoute ->
-                                    popUpTo(startDestinationRoute){
-                                        saveState = true
-                                    }
-                                }
-                                launchSingleTop=true
-                                restoreState = true
-                            }
-                        }
-                    }
-                ) { paddingValues ->
-                    NavBarGraph(navController = navController, innerPadding = paddingValues)
-                }
             }
         }
     }
