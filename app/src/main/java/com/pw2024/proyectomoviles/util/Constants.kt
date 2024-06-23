2f8a5267498ea9c385ba4363df881393f5e7679b
Diego Urbina <00544424@uca.edu.sv>
2024-06-23 15:34:22 -0600

    feat: Add Book and image DTOs

--- app/src/main/java/com/pw2024/proyectomoviles/util/Constants.kt
+++ app/src/main/java/com/pw2024/proyectomoviles/util/Constants.kt
@@ -2,9 +2,25 @@ package com.pw2024.proyectomoviles.util
 
 object Constants {
     //Serialized names
+    //Post
     const val DATE_PUBLISHED = "datePublished"
     const val PUBLISHED_BY = "publishedBy"
     const val BOOK = "book"
     const val TRANSACTION_TYPE = "transactionType"
     const val IMAGE = "image"
+    //Book
+    const val TITLE = "title"
+    const val AUTHOR = "author"
+    const val CONDITION = "condition"
+    const val SYNOPSIS = "synopsis"
+    const val GENRE = "genre"
+    const val YEAR_PUBLISHED = "yearPublished"
+    const val PAGES = "pages"
+    const val ISBN = "isbn"
+    const val PUBLISHER = "publisher"
+    const val LANGUAGE = "language"
+    const val PRICE = "price"
+    //Image
+    const val PUBLIC_ID = "publicId"
+    const val URL = "url"
 }
