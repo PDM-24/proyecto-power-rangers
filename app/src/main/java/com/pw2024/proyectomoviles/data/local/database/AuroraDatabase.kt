c6da45048b31b742476be0d210b48ac83c93b73a
Diego Urbina <00544424@uca.edu.sv>
2024-06-24 19:03:45 -0600

    Add function to get database instance

--- app/src/main/java/com/pw2024/proyectomoviles/data/local/database/AuroraDatabase.kt
+++ app/src/main/java/com/pw2024/proyectomoviles/data/local/database/AuroraDatabase.kt
@@ -1,12 +1,33 @@
 package com.pw2024.proyectomoviles.data.local.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.pw2024.proyectomoviles.data.local.dao.TokenDao
 import com.pw2024.proyectomoviles.data.local.entity.TokenEntity
 
 @Database(
     entities = [TokenEntity::class],
     version = 1
 )
-abstract class AuroraDatabase: RoomDatabase() {
+abstract class AuroraDatabase : RoomDatabase() {
+    abstract fun tokenDao(): TokenDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AuroraDatabase? = null
+
+        fun getDatabase(context: Context): AuroraDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    AuroraDatabase::class.java,
+                    "aurora_db"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
