d1901d4843f1ceef1cd5a7bd144a26fe5349deac
Diego Urbina <00544424@uca.edu.sv>
2024-06-25 01:10:40 -0600

    feat(PostService): add function to get all posts

--- app/src/main/java/com/pw2024/proyectomoviles/data/remote/api/PostService.kt
+++ app/src/main/java/com/pw2024/proyectomoviles/data/remote/api/PostService.kt
@@ -1,9 +1,12 @@
 package com.pw2024.proyectomoviles.data.remote.api
 
+import com.pw2024.proyectomoviles.data.local.entity.TokenEntity
+import com.pw2024.proyectomoviles.data.remote.model.PostDto
 import com.pw2024.proyectomoviles.data.remote.request.LoginRequest
 import com.pw2024.proyectomoviles.data.remote.response.LoginResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
@@ -14,7 +17,7 @@ interface PostService {
 
     @Headers("Content-Type: application/json",)
     @GET("/api/posts/")
-    suspend fun getAllPosts()
+    suspend fun getAllPosts(@Header("Authorization") token: TokenEntity): List<PostDto>
 
     companion object {
         const val BASE_URL = "http://10.0.2.2:3000/"
